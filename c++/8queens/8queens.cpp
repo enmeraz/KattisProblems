25e69264dad31d29434ba39089825c0673f85172
enmeraz <eduardo.meraz12@gmail.com>
2022-09-27 21:43:20 -0600

    update changes

--- c++/8queens/8queens.cpp
+++ c++/8queens/8queens.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main(){
+    string s;
+    int num = 0;
 
-    
+    vector< pair <int,int>> queens;
+
     
     return 0;
 }
